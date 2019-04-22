9f29e18f37bf8b1f89584ad3f14c50cd3c661ff2
shuyi <mycalendula@gmail.com>
2019-04-22 01:56:08 -0400

    W11 finished

---
+++ TAS_W11/Assets/InClassExample/AnimatorParameterController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorParameterController : MonoBehaviour
+{
+    private float walkRun_TreeVal_X;
+    private float walkRun_TreeVal_Y;
+
+    private float time;
+
+    private float idleTime;
+
+    private Animator _myAnimator;
+
+    [Header("Tuning Values")]
+    [Range(0.001f, 10.0f)] public float walkCycleTime;
+    [Range(0.00f, 1.00f)] public float walkRunMagnitude;
+
+    [Range(0.00f, 1.00f)] public float walkRunBlendTotal;
+
+
+    //[Range(0.001f, 10.0f)] public float stepsPerSecond; // 2 * (1/(walkCycleTime)
+
+    //Soh - opposite/hypotenuse
+    //Cah - adjacent/hypotenuse
+    //Toa - opposite/adjacent
+
+    private void Start()
+    {
+        _myAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            _myAnimator.SetBool("Idle_False_Move_True", true);
+        else
+            _myAnimator.SetBool("Idle_False_Move_True", false);
+
+        idleTime += Time.deltaTime * 6;
+        _myAnimator.SetFloat("Idle_TreeVal_X", (Mathf.Sin(idleTime)+1)/2);
+  
+
+
+        //walkCycleTime = 1 - (.5f * walkRunBlendTotal);
+        walkRunMagnitude = .25f + (.75f * walkRunBlendTotal);
+
+        time += (Mathf.PI * 2 * Time.deltaTime) / walkCycleTime;
+
+        walkRun_TreeVal_X = Mathf.Cos(time) * walkRunMagnitude;
+        walkRun_TreeVal_Y = Mathf.Sin(time) * walkRunMagnitude;
+
+        _myAnimator.SetFloat("WalkRun_TreeVal_X", walkRun_TreeVal_X);
+        _myAnimator.SetFloat("WalkRun_TreeVal_Y", walkRun_TreeVal_Y);
+    }
+}
---
+++ TAS_W11/Assets/NewAnimatorController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewAnimatorController : MonoBehaviour
+{
+    
+    private float walkRun_TreeVal_X;
+    private float walkRun_TreeVal_Y;
+
+  
+    private Animator _myAnimator;
+    
+    private float time;
+    private float idleTime;
+    
+    [Header("Tuning Values")]
+    [Range(0.001f, 10.0f)] public float walkCycleTime;
+    [Range(0.00f, 1.00f)] public float walkRunMagnitude;
+
+    [Range(0.00f, 1.00f)] public float walkRunBlendTotal;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        walkCycleTime = 1 - (.2f * walkRunBlendTotal);
+        walkRunMagnitude = .25f + (.75f * walkRunBlendTotal);
+
+        time += (Mathf.PI * 2 * Time.deltaTime) / walkCycleTime;
+
+        walkRun_TreeVal_X = Mathf.Cos(time) * walkRunMagnitude;
+        walkRun_TreeVal_Y = Mathf.Sin(time) * walkRunMagnitude;
+
+        _myAnimator.SetFloat("WalkRun_TreeVal_X", walkRun_TreeVal_X);
+        _myAnimator.SetFloat("WalkRun_TreeVal_Y", walkRun_TreeVal_Y);
+    }
+    
+}
