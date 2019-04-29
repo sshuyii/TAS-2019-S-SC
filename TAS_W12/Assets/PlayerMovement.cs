118f640ad51edd4b67cd021059660df9655d7afd
shuyi <mycalendula@gmail.com>
2019-04-29 01:18:47 -0400

    Week 12

---
+++ TAS_W12/Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 movement;
+    private float x;
+    private float z;
+    private Vector3 relativePos;
+
+    private float timeCount = 0f;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal") * speed;
+        z = Input.GetAxisRaw("Vertical") * speed;
+
+        movement = new Vector3(x, 0, z);
+        movement = Vector3.Normalize(movement);
+        
+
+        transform.position = transform.position + movement;
+        
+        Quaternion rotation = Quaternion.LookRotation(movement, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+        timeCount = timeCount + Time.deltaTime;
+
+    }
+}
